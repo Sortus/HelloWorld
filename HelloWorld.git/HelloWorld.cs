ff195be588a4312e1faf176fe4cefef178f898ee
Sortus <s.kubat@hotmail.de>
2016-07-11 14:46:11 +0200

    ! Unsupported commit message encoding: fatal: ambiguous argument 'HEAD': unknown revision or path not in the working tree. !
    Use '--' to separate paths from revisions, like this:
    'git <command> [<revision>...] -- [<file>...]'

---
+++ HelloWorld.git/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to fuck off");
+            Console.ReadKey();
+        }
+    }
+}
